28df883e02aa21b0c26fa2fa1783d73068cb054d
Carles Capellas <capellas.carles@gmail.com>
2018-02-24 20:50:24 +0100

    Replace plugins by bootstrap template

---
+++ src/components/Projects.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="hello">
+    Projects
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Projects'
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import Projects from '@/components/Projects'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/projects',
+      name: 'Projects',
+      component: Projects
     }
   ]
 })
