7159731ee440096c532b2c489fa268cb1b1ee7d8
Carles Capellas <capellas.carles@gmail.com>
2018-03-10 18:33:40 +0100

    Export a function that recieves middleware as a parameter instead of exporting the router directly

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 var { join } = require('path');
 
-router.get('/*', function (req, res, next) {
-  res.sendFile(join(__dirname, 'dist', 'index.html'));
-});
+const configureRouter = (middleware) => {
+	router.get('/*', function (req, res, next) {
+	  res.sendFile(join(__dirname, 'dist', 'index.html'));
+	});
+	return router;
+}
 
-module.exports = router;
+module.exports = { configureRouter };
