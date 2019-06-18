806136701208603f4357f5cbeccb849cdf05d8f0
Carles Capellas <capellas.carles@gmail.com>
2019-06-18 21:37:35 +0200

    Simplify blog entries creation (not optimal way)

---
+++ src/components/blog-entries/template.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h4>Title</h4>
+        <p>Date</p>
+        <p>
+            Paragraph
+        </p>
+        <p class="text-center">
+            <br />
+            <img class="responsive-image" src="image_url" />
+            <br />
+            <br />
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'article_name'
+    };
+</script>
