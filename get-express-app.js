7b7b6ed4f2cccf80c2df6c2ff411150b8598aa03
Carles Capellas <capellas.carles@gmail.com>
2019-07-17 15:58:43 +0200

    Remove unnecessary express routes

--- get-express-app.js
+++ get-express-app.js
@@ -4,7 +4,5 @@ const { join } = require('path');
 module.exports = () => {
 	const app = express();
 	app.use('/', express.static(join(__dirname, 'dist')));
-	app.get(['/', '/blogs', '/projects', '/trips'], (req, res, next) =>
-	    res.sendFile(join(__dirname, 'dist', 'index.html')));
 	return app;
 };
