b45ae21bc099f497406f4ad8c8da1950d93fbbeb
Carles Capellas <capellas.carles@gmail.com>
2020-04-27 09:18:35 +0200

    chore: standarize npm scripts

--- get-express-app.js
+++ get-express-app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { join } = require('path');
 
 module.exports = () => {
-	const app = express();
-	app.use('/', express.static(join(__dirname, 'dist')));
-	return app;
+    const app = express();
+    app.use('/', express.static(join(__dirname, 'dist')));
+    return app;
 };
--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const getExpressApp = require('./get-express-app');
 
 const app = getExpressApp();
-app.listen(80, error => {
+app.listen(3000, error => {
     if (error) {
         console.log(error);
-    }
-    else {
-        console.log('App 1 running in port 80');
+    } else {
+        console.log('App 1 running in port 3000');
     }
 });
