913d8e520eb7d4f39a850780485718565cf1ef2a
Carles Capellas <capellas.carles@gmail.com>
2020-06-14 19:40:08 +0200

    feat: add webpack spport for mp4 files

--- config/webpack.config.common.js
+++ config/webpack.config.common.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const VueLoaderPlugin      = require('vue-loader/lib/plugin');
-const HtmlPlugin           = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
-const CopyWebpackPlugin    = require('copy-webpack-plugin');
-const path                 = require('path');
-const isDev                = process.env.NODE_ENV === 'development';
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const isDev = process.env.NODE_ENV === 'development';
 
 const envConfig = require('dotenv').config();
 if (envConfig.error || !envConfig.parsed.MAPS_API_KEY || !envConfig.parsed.ANALYTICS_TRACKING_ID) {
@@ -15,16 +15,16 @@ if (envConfig.error || !envConfig.parsed.MAPS_API_KEY || !envConfig.parsed.ANALY
 const webpackConfig = {
     entry: {
         index: path.resolve(__dirname, '..', 'src', 'main'),
-		'editor.worker': 'monaco-editor/esm/vs/editor/editor.worker.js',
-		'json.worker': 'monaco-editor/esm/vs/language/json/json.worker',
-		'css.worker': 'monaco-editor/esm/vs/language/css/css.worker',
-		'html.worker': 'monaco-editor/esm/vs/language/html/html.worker',
-		'ts.worker': 'monaco-editor/esm/vs/language/typescript/ts.worker',
+        'editor.worker': 'monaco-editor/esm/vs/editor/editor.worker.js',
+        'json.worker': 'monaco-editor/esm/vs/language/json/json.worker',
+        'css.worker': 'monaco-editor/esm/vs/language/css/css.worker',
+        'html.worker': 'monaco-editor/esm/vs/language/html/html.worker',
+        'ts.worker': 'monaco-editor/esm/vs/language/typescript/ts.worker'
     },
     resolve: {
-        extensions: [ '.js', '.vue', '.worker' ],
+        extensions: ['.js', '.vue', '.worker'],
         alias: {
-            'vue$': isDev ? 'vue/dist/vue.runtime.js' : 'vue/dist/vue.runtime.min.js',
+            vue$: isDev ? 'vue/dist/vue.runtime.js' : 'vue/dist/vue.runtime.min.js',
             '@': path.resolve(__dirname, '..', 'src')
         }
     },
@@ -33,18 +33,18 @@ const webpackConfig = {
             {
                 test: /\.vue$/,
                 loader: 'vue-loader',
-                include: [ path.resolve(__dirname, '..', 'src') ]
+                include: [path.resolve(__dirname, '..', 'src')]
             },
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
-                include: [ path.resolve(__dirname, '..', 'src') ]
+                include: [path.resolve(__dirname, '..', 'src')]
             },
             {
                 test: /\.css$/,
                 use: [
                     isDev ? 'vue-style-loader' : MiniCSSExtractPlugin.loader,
-                    { loader: 'css-loader', options: { sourceMap: isDev } },
+                    { loader: 'css-loader', options: { sourceMap: isDev } }
                 ]
             },
             {
@@ -59,15 +59,15 @@ const webpackConfig = {
                 test: /\.(woff|woff2|eot|ttf|svg)$/,
                 loader: 'url-loader',
                 options: {
-                    limit: 100000,
-                },
+                    limit: 100000
+                }
             },
             {
-                test: /\.(png|jpe?g|gif)$/i,
+                test: /\.(png|jpe?g|gif|mp4)$/i,
                 loader: 'file-loader',
                 options: {
-                    name: '[path][name].[ext]',
-                },
+                    name: '[path][name].[ext]'
+                }
             },
             {
                 test: require.resolve('jquery'),
@@ -84,8 +84,8 @@ const webpackConfig = {
         }),
         new CopyWebpackPlugin([
             {
-              from: path.resolve(__dirname, '..', 'assets'),
-              to: path.resolve(__dirname, '..', 'dist')
+                from: path.resolve(__dirname, '..', 'assets'),
+                to: path.resolve(__dirname, '..', 'dist')
             }
         ])
     ]
