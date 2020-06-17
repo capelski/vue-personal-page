90f5534032c55f4f60e9e7c6c799fe595b123c84
Carles Capellas <capellas.carles@gmail.com>
2020-06-17 20:42:42 +0200

    feat: expand stadia blog entry

--- src/components/blog/entries/stadia-platform/index.vue
+++ src/components/blog/entries/stadia-platform/index.vue
@@ -2,12 +2,12 @@
     <BlogEntry
         :date="date"
         :description="descriptions[language.current]"
-        duration="TODO"
+        duration="3"
         :entry="entry"
         :isRenderedFromList="isRenderedFromList"
         :languages="entry.languages"
         :allTags="allTags"
-        :tags="[tags.Thoughts]"
+        :tags="[tags.Dissemination]"
         :title="titles[language.current]"
     >
         <english
@@ -30,13 +30,20 @@ import destinyGraphics2 from './destiny-graphics-2.jpg';
 import destinyGraphics3 from './destiny-graphics-3.jpg';
 import destinyGraphics4 from './destiny-graphics-4.jpg';
 import destinyGraphics5 from './destiny-graphics-5.mp4';
+import stadiaDevices from './stadia-devices.png';
+import stadiaGlitch from './stadia-glitch.jpg';
+import stadiaGameStore from './stadia-game-store.png';
+import stadiaLandingPage from './stadia-landing-page.png';
+import stadiaMobile1 from './stadia-mobile-1.jpg';
+import stadiaMobile2 from './stadia-mobile-2.jpg';
+import stadiaSubscription from './stadia-subscription.png';
 
 const date = 'TODO';
 const descriptions = {
-    eng: 'TODO'
+    eng: 'A brief description of the Stadia gaming platform experience'
 };
 const entry = entriesRegistry['stadia-platform'];
-const titles = { eng: 'TODO' };
+const titles = { eng: 'Stadia: cloud gaming' };
 
 export default {
     name: entry.id,
@@ -55,7 +62,14 @@ export default {
                 destinyGraphics2,
                 destinyGraphics3,
                 destinyGraphics4,
-                destinyGraphics5
+                destinyGraphics5,
+                stadiaDevices,
+                stadiaGameStore,
+                stadiaGlitch,
+                stadiaLandingPage,
+                stadiaMobile1,
+                stadiaMobile2,
+                stadiaSubscription
             },
             language,
             tags,
