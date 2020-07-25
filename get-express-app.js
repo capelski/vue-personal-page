2688587936a257300d8956079d7f1316e8a84cf9
Carles Capellas <capellas.carles@gmail.com>
2020-07-25 22:46:14 +0200

    fix: page refresh not working

--- get-express-app.js
+++ get-express-app.js
@@ -4,5 +4,8 @@ const { join } = require('path');
 module.exports = () => {
     const app = express();
     app.use('/', express.static(join(__dirname, 'dist')));
+    app.use('*', (req, res, next) => {
+        return res.sendFile(join(__dirname, 'dist', 'index.html'));
+    });
     return app;
 };
