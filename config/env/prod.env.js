dc99b1a768f66c6d02e5aea0a0faf9f8fcbe05cf
Carles Capellas <capellas.carles@gmail.com>
2021-11-21 11:46:50 +0100

    Update base url (prod.env.js)

--- config/env/prod.env.js
+++ config/env/prod.env.js
@@ -1,6 +1,6 @@
 module.exports = {
     ANALYTICS_TRACKING_ID: process.env.ANALYTICS_TRACKING_ID,
     MAPS_API_KEY: process.env.MAPS_API_KEY,
-    PRODUCTION_URL: 'https://carlescapellas.xyz',
+    PRODUCTION_URL: 'https://capelski.github.io/vue-personal-page',
     NODE_ENV: 'production',
 };
